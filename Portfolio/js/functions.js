a440b8ec7d4ef7fdd043a373806469b35b02c174
Casey O'Kane <okanecasey@gmail.com>
2016-10-27 11:39:44 -0700

    edited some comments

--- Portfolio/js/functions.js
+++ Portfolio/js/functions.js
@@ -3,13 +3,7 @@
 var articles = [];
 
 function Article (options) {
-/* TODO: This is our Model constructor. It will take in
-   our source data from blogArticles and instantiate a
-   new Object according to this new definition. options is
-   a placeholder for the object that will ultimately be
-   passed in. Use all of the properties in blogArticles
-   to populate the new Article data that we'll use.  */
-   /*DONE*/
+
   this.title = options.title;
   this.category = options.category;
   this.author = options.author;
@@ -23,31 +17,18 @@ Article.prototype.toHtml = function() {
   $newArticle.removeClass('template');
 
   $newArticle.attr('data-category', this.category);
-  /* TODO: We also need to fill in:
-  1. author name
-  2. author url
-  3. article title
-  4. article body
-  5. publication*/
-  //done//
+
   $newArticle.find('address a').text(this.author);
   $newArticle.find('section[class="article-body"]').html(this.body);
   $newArticle.find('h1').text(this.title);
   $newArticle.find('address a').attr('href', this.authorUrl);
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
   $newArticle.find('time').text('published' + ' ' + 'about ' + parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000) + ' days ago');
-/* TODO: This cloned article is no longer a template, as it now
-has real data attached to it. Remove the class from this new article! */
-//done//
+
   return $newArticle;
 };
 
-/* This sort method is a standard JavaScript Array function
-   that will iterate over an array and compare its values,
-   and then arrange them in ascending or descending order
-   according to the return value. We are comparing the
-   publishedOn properties to arrange the blog posts in
-   descending order (most recent first). */
+
 blogArticles.sort(function(currentObject, nextObject) {
   return (new Date(nextObject.publishedOn)) - (new Date(currentObject.publishedOn));
 });
